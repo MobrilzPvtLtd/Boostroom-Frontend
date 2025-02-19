357dbe804ef39218eef4ab1a3aebac09674903e9
Devmobrilz <devbrat@mobrilz.agency>
2025-02-19 16:07:47 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import HotGamesSection from "@/component/Home/Home";
+
+import { Geist, Geist_Mono } from "next/font/google"; 
+import { axiosInstance } from "@/utils/axios";
+import Sidebar from "@/component/common/Sidebar";
+import Header from "@/component/common/Header";
+import HeroSection from "@/component/Home/HeroSection";
+import PopularServices from "@/component/Home/PopularServices";
+import TestimonialsSection from "@/component/Home/Testimonials";
+import GiftCardSection from "@/component/Home/GiftCard";
+import TopUpServices from "@/component/Home/TopUpServices";
+import HowItWorksSection from "@/component/Home/HowItWorks";
+import TrendingServices from "@/component/Home/TrendingServices";
+import HotGamesSection from "@/component/Home/HotGames";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,8 +22,40 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+export default function Home ({ services })  {
   return (
- <HotGamesSection />
+    <>
+      <div className="w-full flex">
+        <div className="w-[16%] h-[100vh] overflow-auto custom-scrollbar">
+          <Sidebar services={services} />
+        </div>
+        <div className="w-[84%] h-[100vh] overflow-auto custom-scrollbar">
+          <Header />
+          <div>
+            <HeroSection />
+            <PopularServices />
+            <HotGamesSection />
+            <TestimonialsSection />
+            <GiftCardSection />
+            <TopUpServices />
+            <HowItWorksSection />
+            <TrendingServices />
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+
+// export async function getStaticProps() {
+//   // Fetch data from an API or database
+//   const res = await axiosInstance.get('/services');
+//   const services = await res.data.services
+
+//   return {
+//     props: {
+//       services,
+//     },
+//   };
+// } 
